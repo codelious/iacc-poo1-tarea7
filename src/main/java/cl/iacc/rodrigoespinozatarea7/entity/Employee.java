f89122e570b79eac29a481709afb4056d49774ab
Rodrigo Espinoza <reespinoza@falabella.cl>
2023-05-13 19:50:07 -0400

    updating all for tarea7

--- src/main/java/cl/iacc/rodrigoespinozatarea7/entity/Employee.java
+++ src/main/java/cl/iacc/rodrigoespinozatarea7/entity/Employee.java
@@ -1,14 +1,5 @@
 package cl.iacc.rodrigoespinozatarea7.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 public class Employee {
 
     private String id;
@@ -17,4 +8,46 @@ public class Employee {
     private String position;
     private String email;
 
+    public Employee() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
